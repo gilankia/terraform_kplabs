59aefb1fb62f44c4b5c5be9976aa3ba2bcbb43ca
gilankia <105608952+gilankia@users.noreply.github.com>
2022-12-13 15:39:54 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIARFQ4FXCJFCMVEBWX"
-  secret_key = "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"
+  access_key = "AKIARFQX"
+  secret_key = "r5a6V4Yic5cAyVoiWGFG39"
 }
